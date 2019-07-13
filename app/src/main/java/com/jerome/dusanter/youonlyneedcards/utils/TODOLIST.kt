aed70ecfdf09b5515760aaef17a2704e86c2d473
Jérôme Dusanter <jerome.dusanter@fabernovel.com>
2019-07-13 23:39:49 +0200

    Add TODOLIST

---
+++ app/src/main/java/com/jerome/dusanter/youonlyneedcards/utils/TODOLIST.kt
@@ -0,0 +1,11 @@
+package com.jerome.dusanter.youonlyneedcards.utils
+
+// TODO LIST
+// 1. Changer la sauvegarde : Enlever le bouton et sauvegarder à chaque partie
+// 2. Tester le mode 2 joueurs
+// 3. Checker que les settings sont valides avant de commencer la partie
+// 4. Recave
+// 5. Ajout de joueur pendant la partie
+// 6. Adapter l'UI sur un portable
+// 7. Clean le code au maximim en passant dans chaque classes : Ne laissez que des TODOS concernants la DI
+// 8. Implementer la DI avec Dagger
