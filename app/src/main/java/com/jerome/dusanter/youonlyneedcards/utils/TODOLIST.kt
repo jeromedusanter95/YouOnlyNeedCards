1efa26c398638006f3931e0cde63b0ebe1c5f2e6
Jérôme Dusanter <jerome.dusanter@fabernovel.com>
2019-07-17 22:23:10 +0200

    Add style for CheckBox

--- app/src/main/java/com/jerome/dusanter/youonlyneedcards/utils/TODOLIST.kt
+++ app/src/main/java/com/jerome/dusanter/youonlyneedcards/utils/TODOLIST.kt
@@ -1,7 +1,6 @@
 package com.jerome.dusanter.youonlyneedcards.utils
 
 // TODO LIST
-// 5. Style pour ProfilePlayerView
 // 6. Paramètres pour ajouter de l'argent de manière custom aux joueurs
 // 7. Adapter l'UI sur un portable
 // 8. Clean le code au maximim en passant dans chaque classes : Ne laissez que des TODOS concernants la DI
