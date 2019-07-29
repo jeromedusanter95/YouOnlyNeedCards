01deec2ea419b84eeeee639a6569c287ace8c654
Jérôme Dusanter <jerome.dusanter@fabernovel.com>
2019-07-30 00:45:33 +0200

    Handle wrap_content for heigth of all the recyclerView

--- app/src/main/java/com/jerome/dusanter/youonlyneedcards/utils/TODOLIST.kt
+++ app/src/main/java/com/jerome/dusanter/youonlyneedcards/utils/TODOLIST.kt
@@ -1,8 +1,6 @@
 package com.jerome.dusanter.youonlyneedcards.utils
 
 // TODO LIST
-// 3. Gérer les seekbars pour avoir des chiffres ronds -> DecadeSeekBar
-// 4. Checkez que tous les recyclerView dans les dialogs marchent avec 8 joueurs -> WrapContentRecyclerView
 // 5. Ajouter une page informations sur la WelcomeActivity
 // 6. Modulariser en package
 // 7. Adapter l'UI sur un portable
