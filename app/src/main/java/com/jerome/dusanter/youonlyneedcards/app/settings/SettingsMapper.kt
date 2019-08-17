05e24e21c28a0b78fb6102fee6a0cac3123eae10
Jérôme Dusanter <jerome.dusanter@fabernovel.com>
2019-08-17 11:51:12 +0200

    Clean SettingsMapper

--- app/src/main/java/com/jerome/dusanter/youonlyneedcards/app/settings/SettingsMapper.kt
+++ app/src/main/java/com/jerome/dusanter/youonlyneedcards/app/settings/SettingsMapper.kt
@@ -11,26 +11,16 @@ class SettingsMapper @Inject internal constructor(
 ) {
     fun mapToUiModelSuccess(settings: Settings): SettingsUiModel.Success? {
         return SettingsUiModel.Success(
-            stack = "${settings.stack} " + context.resources.getString(R.string.common_chips),
+            stack = mapToStack(settings),
             isIncreaseBlindsEnabled = settings.isIncreaseBlindsEnabled,
             isMoneyBetEnabled = settings.isMoneyBetEnabled,
-            smallBlind = "${settings.smallBlind}/${settings.smallBlind * 2} "
-                    + context.resources.getString(R.string.common_chips),
-            frequencyIncreasingBlind = "${(settings.frequencyIncreasingBlind / 60000)} "
-                    + context.resources.getString(R.string.common_min),
-            money = "${settings.money} " + context.getString(R.string.common_euros),
-            increaseBlindsAnswer = if (settings.isIncreaseBlindsEnabled) {
-                context.resources.getString(R.string.common_yes)
-            } else {
-                context.resources.getString(R.string.common_no)
-            },
-            moneyBetAnswer = if (settings.isMoneyBetEnabled) {
-                context.resources.getString(R.string.common_yes)
-            } else {
-                context.resources.getString(R.string.common_no)
-            },
-            showErrorMoney = settings.isMoneyBetEnabled && settings.money <= 0,
-            showErrorFrequencyIncreaseBlinds = settings.isIncreaseBlindsEnabled && settings.frequencyIncreasingBlind <= 0,
+            smallBlind = mapToSmallBlind(settings),
+            frequencyIncreasingBlind = mapToFrequencyIncreaseBlinds(settings),
+            money = mapToMoney(settings),
+            increaseBlindsAnswer = mapToIncreaseBlindsAnswer(settings),
+            moneyBetAnswer = mapToMoneyAnswer(settings),
+            showErrorMoney = showErrorMoney(settings),
+            showErrorFrequencyIncreaseBlinds = showErrorFrequencyIncreaseBlinds(settings),
             showErrorStack = settings.stack <= 0,
             showErrorBlinds = settings.smallBlind <= 0
         )
@@ -38,10 +28,46 @@ class SettingsMapper @Inject internal constructor(
 
     fun mapToUiModelError(settings: Settings): SettingsUiModel.Error {
         return SettingsUiModel.Error(
-            showErrorBlinds = settings.smallBlind <= 0,
+            showErrorMoney = showErrorMoney(settings),
+            showErrorFrequencyIncreaseBlinds = showErrorFrequencyIncreaseBlinds(settings),
             showErrorStack = settings.stack <= 0,
-            showErrorFrequencyIncreaseBlinds = settings.isIncreaseBlindsEnabled && settings.frequencyIncreasingBlind <= 0,
-            showErrorMoney = settings.isMoneyBetEnabled && settings.money <= 0
+            showErrorBlinds = settings.smallBlind <= 0
         )
     }
+
+    private fun mapToStack(settings: Settings) =
+        "${settings.stack} " + context.resources.getString(R.string.common_chips)
+
+    private fun mapToSmallBlind(settings: Settings) =
+        "${settings.smallBlind}/${settings.smallBlind * 2} " + context.resources.getString(R.string.common_chips)
+
+    private fun mapToFrequencyIncreaseBlinds(settings: Settings) =
+        ("${(settings.frequencyIncreasingBlind / 60000)} "
+                + context.resources.getString(R.string.common_min))
+
+    private fun mapToMoney(settings: Settings) =
+        "${settings.money} " + context.getString(R.string.common_euros)
+
+    private fun mapToIncreaseBlindsAnswer(settings: Settings): String {
+        return if (settings.isIncreaseBlindsEnabled) {
+            context.resources.getString(R.string.common_yes)
+        } else {
+            context.resources.getString(R.string.common_no)
+        }
+    }
+
+    private fun mapToMoneyAnswer(settings: Settings): String {
+        return if (settings.isMoneyBetEnabled) {
+            context.resources.getString(R.string.common_yes)
+        } else {
+            context.resources.getString(R.string.common_no)
+        }
+    }
+
+    private fun showErrorMoney(settings: Settings) =
+        settings.isMoneyBetEnabled && settings.money <= 0
+
+    private fun showErrorFrequencyIncreaseBlinds(settings: Settings) =
+        settings.isIncreaseBlindsEnabled && settings.frequencyIncreasingBlind <= 0
+
 }
